7452572d1a71603f59523f72c5534e700d7a67d4
Helena <helena.slva19@gmail.com>
2023-08-04 17:27:27 -0300

    Add POO/lista01

---
+++ POO/Lista01/Exercicio01/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Pessoa p1 = new Pessoa();
+        p1.altura = 1.67;
+        p1.peso = 56;
+        p1.calcularImc();
+    }
+}
